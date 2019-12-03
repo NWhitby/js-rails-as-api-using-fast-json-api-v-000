a8e7411963b5f76a71fcc34da65bedfc01b1949a
NaKia Whitby <whitby.nakia@gmail.com>
2019-12-03 17:56:51 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -5,7 +5,9 @@ class SightingsController < ApplicationController
   end
 
   def show
-    sighting = Sighting.find(params[:id])
-    render json: SightingSerializer.new(sighting)
-  end
+    sighting = Sighting.find_by(id: params[:id])
+    options = {
+      include: [:bird, :location]
+    }
+    render json: SightingSerializer.new(sighting, options)
 end
